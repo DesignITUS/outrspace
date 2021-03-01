41d748771ee40cc16dde4faea69480f336ebbc2c
Bui Quang Bao <60503568+buiquangbao@users.noreply.github.com>
2021-03-01 20:49:15 +0700

    Update

---
+++ scripts.js
@@ -0,0 +1,31 @@
+$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    //   obj1
+    if (scroll > 500) {
+        $(".obj1").css("right", - screen.width + scroll - 500);
+    }
+});
+
+var flag = 0;
+const setShadow = () => {
+    let randomnumber = Math.random();
+    let displace = 1.0 + randomnumber * 3.0;
+    var shadow = `${displace}px ${displace / 4}px 0.5px #00bfff60, ${-displace}px ${-displace / 4}px 0.5px #ff320060, 0px 0px 4px #ffffff40`;
+    if (flag == 0 || flag == 1) {
+
+    } else {
+        shadow = `0px 0px 0.5px #00bfff50, 0px 0px 0.5px #ff320050, 0px 0px 4px #ffffff40`;
+    }
+    if (randomnumber > 0.5) {
+        flag = flag + 1;
+    }
+    if (flag == 10) {
+        flag = 0;
+    }
+    var i;
+    for (i = 0; i < 10; i++) {
+        document.getElementsByClassName("glitch-text")[i].style.textShadow = shadow;
+    }
+};
+
+setInterval(setShadow, 200);
